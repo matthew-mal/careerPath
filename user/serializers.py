f597ed33a82b3c549f67f830792c60b23e3b40e0
mat_mal <matvey_maljanov@rambler.ru>
2024-06-21 14:46:23 +0300

    serializers

---
+++ user/serializers.py
@@ -0,0 +1,56 @@
+from .models import Employer, JobSeeker
+from rest_framework import serializers
+
+
+class EmployerListCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employer
+        fields = ['company_name', 'industry', 'other_industry', 'country', 'city']
+
+    def create(self, validated_data):
+        user = Employer(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        for field, value in validated_data.items():
+            if field == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, field, value)
+        instance.save()
+        return instance
+
+
+class JobSeekerListCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobSeeker
+        fields = ['speciality', 'other_speciality']
+
+    def create(self, validated_data):
+        user = JobSeeker(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        for field, value in validated_data.items():
+            if field == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, field, value)
+        instance.save()
+        return instance
+
+
+class ProfileEmployerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employer
+        fields = ['company_name', 'industry', 'other_industry', 'country', 'city']
+
+
+class ProfileJobSeekerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobSeeker
+        fields = ['speciality', 'other_speciality']
