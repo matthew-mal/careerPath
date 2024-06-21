775f47477a7c5caf5fdbdd391408cd2b4017145d
mat_mal <matvey_maljanov@rambler.ru>
2024-06-21 15:37:23 +0300

    verbose_names update

--- user/models.py
+++ user/models.py
@@ -10,8 +10,8 @@ parameters_for_null = {
 
 
 class UserType(models.IntegerChoices):
-    EMPLOYER = 1, "Компания"
-    JOB_SEEKER = 2, "Соискатель"
+    EMPLOYER = 1, "Employer"
+    JOB_SEEKER = 2, "Job Seeker"
 
 
 class MyUserManager(BaseUserManager):
@@ -31,9 +31,9 @@ class MyUserManager(BaseUserManager):
             phone_number=phone_number,
             username=username,
             email=email,
-            password=password  # Добавляем пароль
+            password=password
         )
-        user.is_superuser = True  # Устанавливаем значение is_superuser
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
@@ -42,11 +42,11 @@ class User(AbstractBaseUser):
     user_type = models.PositiveSmallIntegerField(
         choices=UserType.choices,
         default=UserType.JOB_SEEKER,
-        verbose_name="Тип пользователя"
+        verbose_name="Type of the user"
     )
     username = models.CharField(
         max_length=255,
-        verbose_name="Ф.И.О"
+        verbose_name="Full name"
     )
     email = models.EmailField(
         verbose_name="Email"
@@ -54,38 +54,38 @@ class User(AbstractBaseUser):
     phone_number = models.CharField(
         max_length=13,
         unique=True,
-        verbose_name="Номер телефона"
+        verbose_name="Phone number"
     )
     created_date = models.DateTimeField(
         auto_now_add=True,
-        verbose_name="Дата создания"
+        verbose_name="Date of creation"
     )
     updated_date = models.DateTimeField(
         auto_now=True,
-        verbose_name="Дата обновления"
+        verbose_name="Update date"
     )
     status = models.PositiveSmallIntegerField(
         choices=(
-            (1, 'Обычный пользователь'),
-            (2, 'Модератор')
+            (1, 'Base user'),
+            (2, 'Moderator')
         ),
         default=1
     )
     is_admin = models.BooleanField(
         default=False,
-        verbose_name="Админ"
+        verbose_name="Admin"
     )
     is_active = models.BooleanField(
         default=True
     )
     is_staff = models.BooleanField(
         default=False,
-        verbose_name='Персонал'
+        verbose_name='Staff'
     )
 
     class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Все пользователи"
+        verbose_name = "User"
+        verbose_name_plural = "Users"
 
     def __str__(self):
         return self.username
@@ -108,47 +108,47 @@ class User(AbstractBaseUser):
 
 class Employer(User):
     company_name = models.CharField(
-        verbose_name="Название компании",
+        verbose_name="Name of the company",
         max_length=255
     )
     industry = models.CharField(
         max_length=250,
-        verbose_name='Название отрасли',
+        verbose_name='Name of the industry',
         choices=IndustryCh,
         **parameters_for_null
     )
     other_industry = models.CharField(
         max_length=250,
-        verbose_name='Название отрасли(другое)',
+        verbose_name='Name of the industry(another)',
         **parameters_for_null
     )
     country = models.CharField(
-        verbose_name="Страна",
+        verbose_name="Country",
         max_length=255
     )
     city = models.CharField(
-        verbose_name="Город",
+        verbose_name="City",
         max_length=255
     )
 
     class Meta:
-        verbose_name = "Компания"
-        verbose_name_plural = "Компании"
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
 
 
 class JobSeeker(User):
     speciality = models.CharField(
         choices=SpecialityCh,
-        verbose_name="Укажите свою специальность ",
+        verbose_name="Your speciality",
         max_length=255,
         **parameters_for_null
     )
     other_speciality = models.CharField(
-        verbose_name="Укажите свою специальность(другое) ",
+        verbose_name="Your speciality(another)",
         max_length=255,
         **parameters_for_null
     )
 
     class Meta:
-        verbose_name = "Соискатель"
-        verbose_name_plural = "Соискатели"
+        verbose_name = "Job seeker"
+        verbose_name_plural = "Job seekers"
