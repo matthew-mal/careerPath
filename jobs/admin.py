920d8b47692f724f66911e777b2bc3e9e46b37b9
mat_mal <matvey_maljanov@rambler.ru>
2024-06-22 12:49:50 +0300

    admin.register

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,7 +1,51 @@
 from django.contrib import admin
-from .models import Speciality, Experience, Category, Industry
+from .models import (
+    Vacancy, Category, Industry, Chosen,
+    Response, Speciality, Experience
+)
 
-admin.site.register(Speciality)
-admin.site.register(Experience)
-admin.site.register(Category)
-admin.site.register(Industry)
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'salary', 'location', 'status', 'published_date', 'contacts')
+    search_fields = ('location', 'contacts')
+    list_filter = ('status', 'published_date')
+    filter_horizontal = ('industry', 'category', 'specializing', 'experience', 'company')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'other_title')
+    search_fields = ('title', 'other_title')
+
+
+@admin.register(Industry)
+class IndustryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'other_title')
+    search_fields = ('title', 'other_title')
+
+
+@admin.register(Chosen)
+class ChosenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'vacancy', 'created_date')
+    search_fields = ('user__username', 'vacancy__id')
+    list_filter = ('created_date',)
+
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'vacancy', 'created_at')
+    search_fields = ('user__username', 'vacancy__id')
+    list_filter = ('created_at',)
+
+
+@admin.register(Speciality)
+class SpecialityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'other_title')
+    search_fields = ('title', 'other_title')
+
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'other_title')
+    search_fields = ('title', 'other_title')
