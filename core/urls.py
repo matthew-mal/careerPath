32feb5a23d3837869683bc251505f68dbf7a57fc
mat_mal <matvey_maljanov@rambler.ru>
2024-06-22 13:22:08 +0300

    routers and uls

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('user.urls')),
-    # path('jobs/', include('jobs.urls')),
+    path('jobs/', include('jobs.urls')),
 ]
---
+++ jobs/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import VacancyViewSet, ResponseViewSet, ChosenViewSet
+
+router = DefaultRouter()
+router.register(r'vacancies', VacancyViewSet, basename='vacancy')
+router.register(r'responses', ResponseViewSet, basename='response')
+router.register(r'chosen', ChosenViewSet, basename='chosen')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
