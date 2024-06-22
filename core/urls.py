bf8b1f0dc6e913a1a6de345f4419519c19b24eff
mat_mal <matvey_maljanov@rambler.ru>
2024-06-22 14:25:31 +0300

    urls bug fix

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/1/users/', include('user.urls'), namespace='users'),
-    path('api/1/jobs/', include('jobs.urls'), namespace='jobs'),
+    path('api/1/users/', include('user.urls', namespace='users')),
+    path('api/1/jobs/', include('jobs.urls', namespace='jobs')),
 ]
